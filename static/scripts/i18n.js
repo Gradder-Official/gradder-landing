45095fd4afe988ff94dab2aeff569e0f24fc84b6
discordstars <19372006+discordstars@users.noreply.github.com>
2020-08-27 00:35:28 +0100

    feat: :sparkles: BETA i18n feature - language selector still needs work
    CSS and general styles should be update - content doesn't look nice in foreign languages

---
+++ static/scripts/i18n.js
@@ -0,0 +1,65 @@
+let languages = {};
+
+function getParameterCaseInsensitive(object, key) {
+    return object[Object.keys(object).filter(function(k) {
+      return k.toLowerCase() === key.toLowerCase();
+    })[0]];
+  }
+
+function condReplace(element, key, value) {
+    if (element.text().trim().toLowerCase()==key) {
+        element.text(value);
+    }
+}
+
+function loadLang(lang_key) {
+    if (lang_key in languages) {
+        // Use ?v=Date.now() to ensure we never load cached data
+        $.getJSON(languages[lang_key].path + "?v=" + Date.now(), function (data) {
+            $("[data-trans]").each(function (i) {
+                let el = $(this);
+                
+                // Set default to revert back to English
+                if (el.data('phrase') === undefined)
+                    el.attr('data-phrase', el.text());
+                
+                let key = el.text().trim().toLowerCase();
+                key = key.replace( /\s+/g, ' ')
+                let res = getParameterCaseInsensitive(data, key);
+                if (res !== undefined) {
+                    el.text(res);
+                }
+            })
+        })
+    } else if (lang_key === "en") {
+        $("[data-trans]").each(function (i) {
+            let el = $(this);
+            if (el.data('phrase') !== undefined) {
+                el.text(el.data('phrase'));
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+    // Use ?v=Date.now() to ensure we never load cached data
+    $.getJSON('/static/i18n/_index.json?v=' + Date.now(), function (data) {
+        languages = data;
+    }).then(function () {
+        $("#langs").append(`
+        <li><a class="nav-link" href="javascript:loadLang('en')" data-trans>
+        Eng
+        </a></li>
+        `)
+        for (const k in languages) {
+            if (languages.hasOwnProperty(k)) {
+                const language = languages[k];
+                $("#langs").append(`
+                <li><a class="nav-link" href="javascript:loadLang('${k}')" data-trans>
+                ${language.label}
+                </a></li>
+                `)
+            }
+        }
+    })
+})
